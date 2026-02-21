de09d69afb24116fe173ece6d094689dcc6d39dc
Adrian Baido Milante <109199224+adrianmilante12@users.noreply.github.com>
2026-02-21 13:50:29 +0800

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,40 @@
+"use client"
+
+import { useState, useEffect } from "react"
+import Navbar from "@/components/Navbar"
+import FAQ from "@/components/FAQ"
+import LoadingScreen from "@/components/LoadingScreen"
+
+export default function Home() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+      document.body.style.overflow = "auto"
+    }, 2000)
+  }, [])
+
+  return (
+    <>
+      {loading && <LoadingScreen />}
+
+      {!loading && (
+        <>
+          <div className="bg-glow"></div>
+          <Navbar />
+
+          <section className="hero container">
+            <h1>High-Converting Landing Pages</h1>
+            <p>
+              Modern, conversion-focused website templates built for creators,
+              coaches, and online businesses.
+            </p>
+          </section>
+
+          <FAQ />
+        </>
+      )}
+    </>
+  )
+      }
